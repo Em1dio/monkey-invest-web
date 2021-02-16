2827bd3230b952c1d28c70178744a676f51508d3
Alisson Leal <alissonsleal@hotmail.com>
2021-02-16 13:30:34 -0300

    Added prettier and ran 'npm run lint'

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './plugins/axios'
-import VueCookies from 'vue-cookies'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './plugins/axios';
+import VueCookies from 'vue-cookies';
 // https://www.npmjs.com/package/vue-cookies
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
-Vue.use(VueCookies)
+  render: (h) => h(App),
+}).$mount('#app');
+Vue.use(VueCookies);
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,16 +1,16 @@
-import Vue from "vue";
-import axios from "axios";
+import Vue from 'vue';
+import axios from 'axios';
 
-require('dotenv').config()
+require('dotenv').config();
 
 Vue.use({
   install(Vue) {
     Vue.prototype.$http = axios.create({
-      baseURL: "http://localhost:3000",
+      baseURL: 'http://localhost:3000',
       // baseURL: process.env.VUE_APP_NOT_BACKEND_CONFIG ,
-      headers: { 
-        'Content-Type': 'application/json'
-      }
-    })
-  }
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  },
 });
