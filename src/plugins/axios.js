27e3108b2743ba9375a4cf0a545d6ccc83b3eeff
Alisson Leal <alissonsleal@hotmail.com>
2021-03-02 19:53:21 -0300

    updated baseURL

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -7,7 +7,7 @@ Vue.use({
   install(Vue) {
     Vue.prototype.$http = axios.create({
       // baseURL: 'http://0034ce5b2967.ngrok.io',
-      baseURL: 'http://localhost:3000',
+      baseURL: process.env.VUE_APP_BASE_URL,
       // baseURL: process.env.VUE_APP_NOT_BACKEND_CONFIG ,
       headers: {
         'Content-Type': 'application/json',
