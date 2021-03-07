5a3ba19d5f754a29717dfa212e05d28c41e93613
Caio Carnelos <caio.emidio@gmail.com>
2021-03-07 01:07:05 +0000

    update env variables

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -7,8 +7,8 @@ Vue.use({
   install(Vue) {
     Vue.prototype.$http = axios.create({
       // baseURL: 'http://0034ce5b2967.ngrok.io',
-      baseURL: 'http://localhost:3000',
-      // baseURL: process.env.VUE_APP_NOT_BACKEND_CONFIG ,
+      // baseURL: 'http://localhost:3000',
+      baseURL: process.env.VUE_APP_NOT_BACKEND_CONFIG ,
       headers: {
         'Content-Type': 'application/json',
       },
