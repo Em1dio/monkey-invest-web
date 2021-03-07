440141c80e6a5d6dc073b091ae449bff4320ffc2
Caio Carnelos <caio.emidio@gmail.com>
2021-03-07 01:08:05 +0000

    Fix Axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -8,7 +8,7 @@ Vue.use({
     Vue.prototype.$http = axios.create({
       // baseURL: 'http://0034ce5b2967.ngrok.io',
       // baseURL: 'http://localhost:3000',
-      baseURL: process.env.VUE_APP_NOT_BACKEND_CONFIG ,
+      baseURL: process.env.VUE_APP_NOT_BACKEND_CONFIG,
       headers: {
         'Content-Type': 'application/json',
       },
