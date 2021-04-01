8200f78252c843885812764bb30e3b281ab16e25
Caio Carnelos <caio.emidio@gmail.com>
2021-04-01 23:20:18 +0100

    Incluido estrutura basica para funcionar cryptocoins

---
+++ src/views/cryptocoins/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="cryptocoins">
+      <h1>Cryptocoins</h1>
+      <!-- <div class="region">
+        <p class="region__titulo">Insert Stock</p>
+        <insert-stock @inserted-stock="insertStock($event)" />
+      </div>
+      <div class="region">
+        <p class="region__titulo">Stocks</p>
+        <read-stocks
+          :options="true"
+          :stocks="stocks"
+          @removed-stock="removeStock($event)"
+        />
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
